bedb89d42812c09c677824aeced6cabbd7791e69
leanbkhcm <39318027+leanbkhcm@users.noreply.github.com>
2021-06-14 14:51:19 +0700

    Update Routes_org.js

--- web/src/Routes_org.js
+++ web/src/Routes_org.js
@@ -1,4 +1,5 @@
 // web/src/Routes.js
+// <Route path="/" page={HomePage} name="home" />
 
 import { Router, Route, Set } from '@redwoodjs/router'
 import PostsLayout from 'src/layouts/PostsLayout'
@@ -7,6 +8,8 @@ import BlogLayout from 'src/layouts/BlogLayout'
 const Routes = () => {
   return (
     <Router>
+    <Route path="/" page={StartPage} name="start" />
+    
       <Set wrap={PostsLayout}>
         <Route path="/posts/new" page={NewPostPage} name="newPost" />
         <Route path="/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
@@ -15,7 +18,7 @@ const Routes = () => {
       </Set>
       <Set wrap={BlogLayout}>
         <Route path="/about" page={AboutPage} name="about" />
-        <Route path="/" page={HomePage} name="home" />
+       
       </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
