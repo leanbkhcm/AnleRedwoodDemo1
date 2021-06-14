5fbe1324c732910ffc4becc9fe766c6065d9b7ad
leanbkhcm <39318027+leanbkhcm@users.noreply.github.com>
2021-06-14 15:29:04 +0700

    Update PageView.ts

--- web/src/PageView.ts
+++ web/src/PageView.ts
@@ -1,5 +1,13 @@
-import ReactGA from 'react-ga'
+//import ReactGA from 'react-ga'
 
 export const GAPageView = () => {
-  ReactGA.pageview(window.location.pathname + window.location.search)
+  //ReactGA.pageview(window.location.pathname + window.location.search)
+  (
+    <div style={{ height: "100%" }}>
+      <div className="text-center text-gray-700 font-medium">{props.chartTitle}</div>
+      <div style={{ height: responsiveHeight }}>
+
+      </div>
+    </div>
+  )
 }
