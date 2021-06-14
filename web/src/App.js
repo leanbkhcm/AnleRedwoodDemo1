65905a3bda39304ed04e2430509152be2b7f1ff2
leanbkhcm <39318027+leanbkhcm@users.noreply.github.com>
2021-06-14 10:11:08 +0700

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -4,8 +4,8 @@ import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
-//import './scaffold.css'     //anle temp remove
-//import './index.css'         //anle temp remove
+import './scaffold.css'   
+import './index.css'        
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
