dc6d910f02450bdf4d800ed186186d2d106f486e
leanbkhcm <39318027+leanbkhcm@users.noreply.github.com>
2021-06-14 19:54:16 +0700

    Delete PageHelper.ts

--- web/src/PageHelper.ts
+++
@@ -1,7 +0,0 @@
-export const setPageTitle = (title: string) => {
-  if (title) {
-    document.title = title + " - Vida"
-  } else {
-    document.title = "Vida - Data Visualization Made Easy"
-  }
-}
